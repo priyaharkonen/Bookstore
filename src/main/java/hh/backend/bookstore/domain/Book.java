849241357b675278d8fe28bad6ccb9d14e7fc6a1
priyaharkonen <bhy827@myy.haaga-helia.fi>
2026-09-17 11:42:35 +0300

    First commit

---
+++ src/main/java/hh/backend/bookstore/domain/Book.java
@@ -0,0 +1,81 @@
+package hh.backend.bookstore.domain;
+
+public class Book {
+
+    //attribuutit
+    private String title;
+    private String author;
+    private int publicationYear;
+    private int isbn;
+    private double price;
+
+    //Konstruktiorit
+    public Book() {
+        super();
+        this.title = null;
+        this.author = null;
+        this.publicationYear = 0;
+        this.isbn = 0;
+        this.price = 0;
+    }
+
+    public Book(String title, String author, int publicationYear, int isbn, double price) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.isbn = isbn;
+        this.price = price;
+    }
+    
+    //setterit
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    //getterit
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    //toString()
+    @Override
+    public String toString() {
+        return "Book [title=" + title + ", author=" + author + ", publicationYear=" + publicationYear + ", isbn=" + isbn
+                + ", price=" + price + "]";
+    }
+    
+
+}
+
---
+++ src/main/java/hh/backend/bookstore/web/BookController.java
@@ -0,0 +1,7 @@
+package hh.backend.bookstore.web;
+
+//Request: http://localhost:8080/index
+
+public class BookController {
+
+}
