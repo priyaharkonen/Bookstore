b0347860997a14abb1675abb0a8ac42b014daa23
priyaharkonen <bhy827@myy.haaga-helia.fi>
2026-09-17 11:59:19 +0300

    First Changes

--- src/main/java/hh/backend/bookstore/web/BookController.java
+++ src/main/java/hh/backend/bookstore/web/BookController.java
@@ -1,7 +1,27 @@
 package hh.backend.bookstore.web;
 
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import hh.backend.bookstore.domain.Book;
+
+
 //Request: http://localhost:8080/index
 
+@Controller 
 public class BookController {
+
+    //Kirjat -etusivu (index)
+    @RequestMapping(value = "/index", method=RequestMethod.GET)
+    public String getBooks(Model model) {
+        model.addAttribute("book", new Book());
+        return "bookform";
+    }
+    
 
 }
